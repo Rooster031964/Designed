b21ca343695e8961b880b4364f9148a1cd0f05e9
Rooster031964 <71811953+Rooster031964@users.noreply.github.com>
2020-09-24 09:00:50 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+for (let index = 0; index < 41000000; index++) {
+    basic.setLedColor(0xff0000)
+    basic.pause(100)
+    basic.setLedColor(0xff8000)
+    basic.pause(100)
+    basic.setLedColor(0xffff00)
+    basic.pause(100)
+    basic.setLedColor(0xff9da5)
+    basic.pause(100)
+    basic.setLedColor(0x00ff00)
+    basic.pause(100)
+    basic.setLedColor(0xb09eff)
+    basic.pause(100)
+    basic.setLedColor(0x00ffff)
+    basic.pause(100)
+}
 basic.forever(function () {
     basic.showLeds(`
         # # . . .
